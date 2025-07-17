37efa492721348370007d39ccabc6eb9f72130ce
DaauShen <mrdu2004@gmail.com>
2025-07-17 15:55:09 +0700

    minor fix

--- shoumeisho-cozy-house/src/app/birthday/page.tsx
+++ shoumeisho-cozy-house/src/app/birthday/page.tsx
@@ -11,7 +11,7 @@ export default function SuKienPage() {
     <main className="p-4 sm:p-6 md:p-8 max-w-7xl mx-auto">
       <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center">Lịch sinh nhật của Vocal Synth</h1>
       <p className="text-gray-700 mb-6 text-center">
-        Ấn vào một tháng bất kì để xem sinh nhật của các Vocaloid trong tháng đó. Bạn cũng có thể tìm kiếm ngày sinh nhật theo tên của Vocal Synth.
+        Ấn vào một tháng bất kì để xem sinh nhật của các Vocal Synth trong tháng đó. Bạn cũng có thể tìm kiếm ngày sinh nhật theo tên của Vocal Synth.
       </p>
       <BirthdayCalendar/>
     </main>
