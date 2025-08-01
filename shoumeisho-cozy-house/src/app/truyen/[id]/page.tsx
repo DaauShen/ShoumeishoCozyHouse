8debc8d253ae2d3bf1168b646cc24a5982267923
DaauShen <mrdu2004@gmail.com>
2025-08-01 11:25:08 +0700

    fix

--- shoumeisho-cozy-house/src/app/truyen/[id]/page.tsx
+++ shoumeisho-cozy-house/src/app/truyen/[id]/page.tsx
@@ -1,18 +1,15 @@
 'use client'
 
 import ComicDetail from '@/features/manga/components/ComicDetail'
-import { useComicById, useDeleteChapter } from '@/features/manga/query/queries'
+import { useComicById } from '@/features/manga/query/queries'
 import { useAuth } from '@/providers/AuthProvider'
 import { useParams, useRouter } from 'next/navigation'
-import { useState } from 'react'
 
 export default function MangaDetailPage() {
   const { id } = useParams()
   const { user } = useAuth()
   const router = useRouter()
   const { data: comic, isLoading, refetch } = useComicById(id as string)
-  const [chapterToDelete, setChapterToDelete] = useState<number | null>(null)
-  const deleteMutation = useDeleteChapter(id as string, chapterToDelete ?? -1)
 
   if (isLoading) {
     return <div className="text-center text-[#80C6EA] font-vi">Đang tải...</div>
